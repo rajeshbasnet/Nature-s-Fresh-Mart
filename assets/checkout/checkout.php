a2f461e476a53bde75d47cf5f942cc4e15629efb
rajesh basnet <50390782+rajeshbasnet@users.noreply.github.com>
2021-06-29 16:07:42 +0545

    commit chamnges

--- assets/checkout/checkout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-session_start();
-
-if(isset($_SESSION['user'])) {
-
-}else {
-    header('Location: /website/project/assets/form/signin/signin.php?message=login');
-}
--- includes/cdn-links/fontawesome-cdn.php
+++
@@ -1,5 +0,0 @@
-<!--Fontawesome CDN-->
-<script
-        src="https://kit.fontawesome.com/962cfbd2be.js"
-        crossorigin="anonymous" defer
-></script>
