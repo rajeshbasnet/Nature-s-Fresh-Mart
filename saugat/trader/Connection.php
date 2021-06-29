38d4b891223ab122222478e9b29f212dd51e9be0
vileroze <50352881+vileroze@users.noreply.github.com>
2021-06-29 14:00:40 +0545

    DELETE  BHAYO

--- saugat/trader/Connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $connection = oci_pconnect("ECOMMERCE", "vileroze", "localhost/xe");
-    if (!$connection) {
-      $error =oci_error();
-      echo "string";
-      trigger_error('Could not connect to database: '.$error['message'],E_USER_ERROR);
-     } echo "";
-
-?>
