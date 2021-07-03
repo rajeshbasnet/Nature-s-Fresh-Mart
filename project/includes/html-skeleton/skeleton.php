e42a8048a3c288616e011f54c84b5fa251ed7591
vileroze <50352881+vileroze@users.noreply.github.com>
2021-07-03 20:36:45 +0545

    chalne wala

---
+++ project/includes/html-skeleton/skeleton.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <title>Nature's Fresh Mart</title>
+</head>
+
+<body>
