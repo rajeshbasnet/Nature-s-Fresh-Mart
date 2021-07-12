43f02bbe5a0cb4e63381d71c4e9a4d5a1e899ab4
rajesh basnet <50390782+rajeshbasnet@users.noreply.github.com>
2021-07-12 20:20:57 +0545

    delete v2.1

--- project/panels/script.js
+++
@@ -1,7 +0,0 @@
-//Display logout btn when users clicks on profile icon on a panel
-profileIcon = document.querySelector('main .container-fluid .row .col-xl-10 .user-profile-header img');
-logoutSection = document.querySelector('main .container-fluid .row .col-xl-10 .logout-section');
-
-profileIcon.addEventListener('click', () => {
-    logoutSection.classList.toggle('block');
-})
